09d9976444dfc37e88f816f612c79e92117b1455
thediep094 <thediep094@gmail.com>
2023-02-28 23:26:11 +0700

    update footer, button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,81 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
-function Button() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [isHovering, setIsHovering] = useState<boolean>(false);
-  useEffect(() => {
-    if (!isHovering) {
-      const canvas = canvasRef.current;
-      if (canvas) {
-        const ctx = canvas.getContext("2d");
-        if (!ctx) {
-          return;
-        }
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // Đặt màu cho đường vẽ
-        ctx.strokeStyle = "#fff";
+import React from "react";
+import "../../styles/components/button.scss";
+type TProp = {
+  name: String;
+};
 
-        // Vẽ đường viền
-        ctx.beginPath();
-        ctx.moveTo(15, 0);
-        ctx.lineTo(195, 0);
-        ctx.lineTo(195, 51);
-        ctx.lineTo(180, 66);
-        ctx.lineTo(0, 66);
-        ctx.lineTo(0, 15);
-        ctx.closePath();
-        ctx.stroke();
-      }
-    } else {
-      const canvas = canvasRef.current;
-      if (canvas) {
-        const ctx = canvas.getContext("2d");
-        if (!ctx) {
-          return;
-        }
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // Đặt màu cho đường vẽ
-        ctx.strokeStyle = "#fff";
-        // Vẽ đường viền
-        ctx.beginPath();
-        ctx.moveTo(15, 0);
-        ctx.lineTo(195, 0);
-
-        ctx.lineTo(195, 66);
-
-        ctx.lineTo(180, 66);
-        ctx.lineTo(0, 66);
-        ctx.lineTo(0, 0);
-        ctx.closePath();
-        ctx.stroke();
-      }
-    }
-  }, [isHovering]);
-
-  const handleMouseEnter = () => {
-    setIsHovering(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovering(false);
-  };
-  console.log(isHovering);
+const Button = (prop: TProp): any => {
   return (
-    <div
-      className="button"
-      onMouseEnter={() => handleMouseEnter()}
-      onMouseLeave={() => handleMouseLeave()}
-    >
-      <canvas
-        ref={canvasRef}
-        className="canvas-button"
-        width={195}
-        height={66}
-        style={{
-          transition: `all .3s ease-in-out`,
-        }}
-      />
+    <div className="button">
+      <div className="button-content">{prop.name}</div>
     </div>
   );
-}
+};
 
 export default Button;
