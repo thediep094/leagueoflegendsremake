6ad8bdd1e5d4f6d81ee87e369160c3bde63c7e2f
thediep094 <thediep094@gmail.com>
2023-03-07 21:32:32 +0700

    update profile page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Champions from "./pages/Champions";
+import Search from "./pages/Search";
+import UserInformation from "./pages/User";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route index path="/" element={<Homepage />} />
           <Route path="/champions" element={<Champions />} />
           <Route path="/news" element={<div>News</div>} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/user/:username" element={<UserInformation />} />
         </Routes>
       </BrowserRouter>
     </div>
