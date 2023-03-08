6295c1ba42391db9afe54051e52bf1c6dff05750
Phạm Anh Hiếu <HIEUPA.B19CN254@stu.ptit.edu.vn>
2023-03-08 10:23:58 +0700

    update get user

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -16,6 +16,26 @@ const UserController = {
       });
     }
   },
+  getUser: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const data = await User.findById(id);
+      if (data) {
+        return res.status(200).json({
+          message: "Thành công",
+          user: data,
+        });
+      }
+      return res.status(404).json({
+        message: "Người dùng không tồn tại",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Server error",
+        error: error,
+      });
+    }
+  },
 };
 
 module.exports = UserController;
