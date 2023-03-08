ba7fd813bbf14dc2cec7a4c7551943cb09426cc6
Phạm Anh Hiếu <HIEUPA.B19CN254@stu.ptit.edu.vn>
2023-03-08 10:28:07 +0700

    add login middleware

--- server/middleware/user.middleware.js
+++ server/middleware/user.middleware.js
@@ -10,6 +10,15 @@ const userMiddleware = {
     }
     next();
   },
+  checkRequiredLogin: async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Điền thiếu thông tin, vui lòng nhập đầy đủ thông tin",
+      });
+    }
+    next();
+  },
 };
 
 module.exports = userMiddleware;
