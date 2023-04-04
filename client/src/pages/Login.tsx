1158796a8062ee61f27256ee37fba8b9542ac855
huybq_107 <64139999+huybq100701@users.noreply.github.com>
2023-04-04 19:27:49 +0700

    update

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,33 +1,156 @@
-import React from 'react'
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { Component } from "react";
+import { Navigate } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import "../styles/pages/Login.scss"
+import AuthService from "../services/auth.service";
 
-const Login = () => (
-  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='teal' textAlign='center'>
-        <Image src='/logo.png' /> Log-in to your account
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
+type Props = {};
+
+type State = {
+  redirect: string | null,
+  username: string,
+  password: string,
+  loading: boolean,
+  message: string
+};
+
+export default class Login extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+
+    this.state = {
+      redirect: null,
+      username: "",
+      password: "",
+      loading: false,
+      message: ""
+    };
+  }
+
+  componentDidMount() {
+    const currentUser = AuthService.getCurrentUser();
+
+    if (currentUser) {
+      this.setState({ redirect: "/profile" });
+    };
+  }
+
+  componentWillUnmount() {
+    window.location.reload();
+  }
+
+  validationSchema() {
+    return Yup.object().shape({
+      username: Yup.string().required("This field is required!"),
+      password: Yup.string().required("This field is required!"),
+    });
+  }
+
+  handleLogin(formValue: { username: string; password: string }) {
+    const { username, password } = formValue;
+
+    this.setState({
+      message: "",
+      loading: true
+    });
+
+
+    AuthService.login(username, password).then(
+      () => {
+        this.setState({
+          redirect: "/profile"
+        });
+      },
+      error => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        this.setState({
+          loading: false,
+          message: resMessage
+        });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Navigate to={this.state.redirect} />
+    }
+
+    const { loading, message } = this.state;
+
+    const initialValues = {
+      username: "",
+      password: "",
+    };
+
+    return (
+      <div className="col-md-12">
+        <img
+          src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gamedeveloper.com%2Fpc%2Friot-games-to-self-publish-i-league-of-legends-i-and-i-teamfight-tactics-i-in-southeast-asia&psig=AOvVaw1Sy4g0iZ1220Hc1KRG318c&ust=1680697247994000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIjrqaWbkP4CFQAAAAAdAAAAABAZ"
+          alt="login-bg"
+          className="login-img"
+        />
+        <div className="card card-container">
+          <img
+            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+            alt="profile-img"
+            className="profile-img-card"
           />
 
-          <Button color='teal' fluid size='large'>
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        New to us? <a href='#'>Sign Up</a>
-      </Message>
-    </Grid.Column>
-  </Grid>
-)
+          <Formik
+            initialValues={initialValues}
+            validationSchema={this.validationSchema}
+            onSubmit={this.handleLogin}
+          >
+            <Form>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <Field name="username" type="text" className="form-control" />
+                <ErrorMessage
+                  name="username"
+                  component="div"
+                  className="alert alert-danger"
+                />
+              </div>
 
-export default Login
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <Field name="password" type="password" className="form-control" />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="alert alert-danger"
+                />
+              </div>
+
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary btn-block" disabled={loading}>
+                  {loading && (
+                    <span className="spinner-border spinner-border-sm"></span>
+                  )}
+                  <span>Login</span>
+                </button>
+              </div>
+
+              {message && (
+                <div className="form-group">
+                  <div className="alert alert-danger" role="alert">
+                    {message}
+                  </div>
+                </div>
+              )}
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    );
+  }
+}
