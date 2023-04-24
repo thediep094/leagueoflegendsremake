64e5de78bde765656f80357a8e1bce63abf27fda
Tien Dung <tiendung.2001.dev@gmail.com>
2023-04-24 12:00:37 +0700

    update create product

--- server/controller/product.controller.js
+++ server/controller/product.controller.js
@@ -1,21 +1,55 @@
 const Product = require("../model/Product");
+const ProductImages = require("../model/ProductImages");
+const ThumnailImage = require("../model/ThumnailImage");
 const mongoose = require("mongoose");
 
 
 const ProductController = {
 
     create: async (req, res) => {
-//     {
-//     "name": "Test product",
-//     "price": 1234.56,
-//     "compare_at_price": 1500.00,
-//     "description": "Test des",
-//     "img": "Test img",
-//     "estimate_ship_date": "",
-//     "tagId": ["1", "2"]
-// }
+        // {
+        //     "name": "Test product",
+        //     "price": 1234.56,
+        //     "compare_at_price": 1500.00,
+        //     "description": "Test des",
+        //     "img": "Test img",
+        //     "estimate_ship_date": "",
+        //     "tagId": ["1", "2"],
+        //     "images": [
+        //         {
+        //             "img": "Test img 1",
+        //             "alt": "test alt 1"
+        //         },
+        //         {
+        //             "img": "Test img 2",
+        //             "alt": "test alt 2"
+        //         }
+        //     ],
+        //     "thumnails": [
+        //         {
+        //             "img": "Test img 1",
+        //             "alt": "test alt 1"
+        //         },
+        //         {
+        //             "img": "Test img 2",
+        //             "alt": "test alt 2"
+        //         }
+        //     ]
+        // }
         try {
             const newProduct = await Product.create(req.body);
+            const images = req.body.images;
+            const thumnails = req.body.thumnails;
+            for (var image of images) {
+                image.productId = newProduct._id;
+                console.log(image);
+                const newProductImages = await ProductImages.create(image);
+            }
+
+            for (var thumnail of thumnails) {
+                thumnail.productId = newProduct._id;
+                const newThumnailImages = await ThumnailImage.create(thumnail);
+            }
             return res.status(200).json({
                 message: "Tạo sản phẩm thành công",
                 product: newProduct,
