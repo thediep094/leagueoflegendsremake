501cef6fd1d717ecf22cff2bafc74512d107e9f9
Hieu-PhamAnh <hieupa.b19cn254@stu.ptit.edu.vn>
2023-04-29 22:55:30 +0700

    update full cart

--- server/controller/cart.controller.js
+++ server/controller/cart.controller.js
@@ -9,7 +9,7 @@ const CartController = {
         try {
             const id = req.body.verify_id;
             let cart = await createCart(id);
-            console.log(cart);
+            // console.log(cart);
             if (!cart) {
                 return res.status(500).json({
                     message: "Server error",
@@ -42,27 +42,32 @@ const CartController = {
                         updatedAt: 0,
                         __v: 0,
                         "list._id": 0,
-                        // "productList.images": 0,
-                        // "productList.thumbnails": 0,
-                        // "productList.images._id": 0,
-                        // "productList.createdAt": 0,
-                        // "productList.updatedAt": 0,
-                        // "productList.__v": 0,
+                        "productList.description": 0,
+                        "productList.thumbnails": 0,
+                        "productList.createdAt": 0,
+                        "productList.updatedAt": 0,
+                        "productList.__v": 0,
                         // "productList.hello": 0,
-                        // "productList.test": {
-                        //     $arrayElemAt: ["$productList.images", 0],
-                        // },
                     },
                 },
             ];
-            const data = await Cart.aggregate(pipeline); // bị ExceededMemoryLimit tại đây
-            console.log(data[0].productList);
-            data[0].productList[0].images = data[0].productList[0].images[0];
-            // console.log("images: " + data[0].productList[0].images.length);
-            // console.log("hello: " + data[0].productList[0].hello.length);
+            let data = await Cart.aggregate(pipeline); // bị ExceededMemoryLimit tại đây nếu như lưu ảnh bằng cách cũ
+            if (cart.list.length > 0) {
+                data = data[0];
+            }
+            // 2 vòng for lồng nhau để thêm quantity vào trong productList
+            for (let product of data.productList) {
+                product.images = product.images[0];
+                data.list.forEach((item) => {
+                    if (item.productID.equals(product._id)) {
+                        product.quantity = item.quantity;
+                    }
+                });
+            }
+            delete data.list;
             return res.status(200).json({
                 message: "Get cart thành công",
-                cart: data[0],
+                cart: data,
             });
         } catch (error) {
             return res.status(500).json({
@@ -145,11 +150,170 @@ const CartController = {
         }
     },
     // add mới một product by id: nếu có thì +1, ko thì thêm
-    addProductByID: async (req, res) => {},
+    addProductByID: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { productID } = req.body;
+            if (!mongoose.Types.ObjectId.isValid(productID)) {
+                return res.status(400).json({
+                    message: "product id không hợp lệ",
+                });
+            }
+            let cart = await Cart.aggregate([
+                {
+                    $match: {
+                        _id: new mongoose.Types.ObjectId(id),
+                    },
+                },
+            ]);
+            if (cart.length == 0) {
+                return res.status(404).json({
+                    message: "Cart không tồn tại",
+                });
+            }
+            cart = cart[0];
+            let check = true;
+            cart.list.forEach((item) => {
+                if (item.productID == productID) {
+                    item.quantity += 1;
+                    check = false;
+                }
+            });
+            if (check) {
+                cart.list.push({
+                    productID: productID,
+                    quantity: 1,
+                });
+            }
+            const saveCart = await Cart.findByIdAndUpdate(
+                cart._id,
+                { list: cart.list },
+                {
+                    new: true,
+                },
+            );
+            return res.status(200).json({
+                message: "Thêm product vào cart thành công",
+                cart: saveCart,
+            });
+        } catch (error) {
+            return res.status(500).json({
+                message: "Server error",
+                error: error,
+            });
+        }
+    },
     // update product số lượng by id:
-    updateProductQuantityByID: async (req, res) => {},
+    updateProductQuantityByID: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { productID, quantity } = req.body;
+            if (!mongoose.Types.ObjectId.isValid(productID)) {
+                return res.status(400).json({
+                    message: "product id không hợp lệ",
+                });
+            }
+            let cart = await Cart.aggregate([
+                {
+                    $match: {
+                        _id: new mongoose.Types.ObjectId(id),
+                    },
+                },
+            ]);
+            if (cart.length == 0) {
+                return res.status(404).json({
+                    message: "Cart không tồn tại",
+                });
+            }
+            cart = cart[0];
+            let check = true;
+            cart.list.forEach((item) => {
+                if (item.productID == productID) {
+                    item.quantity = quantity;
+                    check = false;
+                }
+            });
+            if (check) {
+                return res.status(400).json({
+                    message: "Không tồn tại product trong cart",
+                });
+            }
+            const saveCart = await Cart.findByIdAndUpdate(
+                cart._id,
+                { list: cart.list },
+                {
+                    new: true,
+                },
+            );
+            return res.status(200).json({
+                message: "Update product quantity thành công",
+                cart: saveCart,
+            });
+        } catch (error) {
+            return res.status(500).json({
+                message: "Server error",
+                error: error,
+            });
+        }
+    },
     // xóa product trong cart by id:
-    deleteProductByID: async (req, res) => {},
+    deleteProductByID: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { productID } = req.body;
+            if (!mongoose.Types.ObjectId.isValid(productID)) {
+                return res.status(400).json({
+                    message: "product id không hợp lệ",
+                });
+            }
+            let cart = await Cart.aggregate([
+                {
+                    $match: {
+                        _id: new mongoose.Types.ObjectId(id),
+                    },
+                },
+            ]);
+            if (cart.length == 0) {
+                return res.status(404).json({
+                    message: "Cart không tồn tại",
+                });
+            }
+            cart = cart[0];
+            let check = true;
+            let index = -1;
+            cart.list.forEach((item) => {
+                if (item.productID == productID) {
+                    index = cart.list.indexOf(item);
+                    check = false;
+                }
+            });
+            if (check) {
+                return res.status(400).json({
+                    message: "Không tồn tại product trong cart",
+                });
+            }
+            // console.log("index: " + index);
+            // console.log("vẫn chạy 1");
+            cart.list.splice(index, 1);
+            // console.log("vẫn chạy 2");
+            const saveCart = await Cart.findByIdAndUpdate(
+                cart._id,
+                { list: cart.list },
+                {
+                    new: true,
+                },
+            );
+            return res.status(200).json({
+                message: "Update product quantity thành công",
+                cart: saveCart,
+            });
+        } catch (error) {
+            return res.status(500).json({
+                message: "Server error",
+                error: error,
+            });
+        }
+    },
 };
 
 module.exports = CartController;
