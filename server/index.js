1bb2317b2a692187d5fa56e57f74b73a25999274
thediep094 <thediep094@gmail.com>
2023-05-01 20:24:08 +0700

    dadas

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
-const { Server } = require("socket.io");
 const app = express();
 const http = require("http").Server(app);
 const port = process.env.PORT;
@@ -30,29 +29,6 @@ app.get("/", (req, res) => {
     res.send("Hello World!?");
 });
 
-const io = require("socket.io")(http, {
-    cors: {
-        origin: "*",
-    },
-});
-io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-
-    socket.on("join_room", (data) => {
-        socket.join(data);
-        console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    });
-
-    socket.on("send_message", (data) => {
-        console.log(data);
-        socket.broadcast.emit("receive_alert", data);
-        socket.to(data.room).emit("receive_message", data);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("User Disconnected", socket.id);
-    });
-});
 http.listen(port, () => {
     console.log(`Server listening on ${port}`);
 });
