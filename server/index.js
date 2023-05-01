efffc43a6e2be39ae5ea2f3634758198011a4254
thediep094 <thediep094@gmail.com>
2023-05-01 12:26:33 +0700

    update cors

--- server/index.js
+++ server/index.js
@@ -35,10 +35,11 @@ const server = app.listen(port, () => {
 });
 
 const io = new Server(server, {
-    cors: {
-        origin: `http://localhost:3000`,
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+  allowEIO3: true,
 });
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
